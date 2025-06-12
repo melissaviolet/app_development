e34b2985859ec77447096a749f3b1de7e430472b
Melissa Violet <melissavioletvs@gmail.com>
2025-06-12 16:00:05 +0300

    Added a profile card for the start

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,91 @@ class WhatsApp extends StatelessWidget {
         title: Text(
           "WhatsApp",
           style: TextStyle(
-            color:Colors.white,
-            fontSize:40,
-            fontWeight:FontWeight.bold,
+            color: Colors.white,
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
           ),
         ),
         actions: [
           IconButton(
             icon: Icon(Icons.search),
             color: Colors.white,
-            onPressed: () {
-              
-            },
-          
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.camera_alt),
             color: Colors.white,
-            onPressed: () {
-            },
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.more_vert),
             color: Colors.white,
-            onPressed: () {
-            },
+            onPressed: () {},
           ),
-
         ],
       ),
-    );
 
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://c4.wallpaperflare.com/wallpaper/606/692/865/anime-anime-girls-artwork-car-vehicle-hd-wallpaper-preview.jpg',
+                ),
+                radius: 40.0,
+              ),
+            ),
+
+            Divider(height: 60.0, color: Colors.cyan[200]),
+            Text(
+              "NAME",
+              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "Melissa",
+              style: TextStyle(
+                color: Colors.purple[300],
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "CURRENT LEVEL",
+              style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "5",
+              style: TextStyle(
+                color: Colors.purple[300],
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Row(
+              children: [
+                Icon(Icons.email, color: Colors.purple[400]),
+                SizedBox(width: 10.0),
+                Text(
+                  "melissa@example.com",
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
-}
+} 
