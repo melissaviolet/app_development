cd10c5b441f20cf59bd81eee7c7660121ccbf9e6
Melissa Violet <melissavioletvs@gmail.com>
2025-07-10 17:54:29 +0300

    Updated reader dispatcher to accept a config argument

--- reader_dispatcher.py
+++ reader_dispatcher.py
@@ -1,15 +1,10 @@
-import json
 from readers.reader_csv import read_sales_from_csv
 from readers.reader_sqlite import read_sales_from_sqlite
 from readers.reader_mysql import read_sales_from_mysql
 from readers.reader_api import read_sales_from_api
 
-def read_sales():
-    with open("config.json") as f:
-        config = json.load(f)
-
-    src = config["source_type"]
-
+def read_sales(config):
+    src = config.get("source_type")
     if src == "csv":
         return read_sales_from_csv(config["csv"]["file_path"])
     elif src == "sqlite":
