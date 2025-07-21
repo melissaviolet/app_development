86229c87b1ec6bdeb1438ce22c271ec20398468e
Melissa Violet <melissavioletvs@gmail.com>
2025-07-21 17:57:50 +0300

    Field mapping in csv reader

--- readers/reader_csv.py
+++ readers/reader_csv.py
@@ -1,11 +1,14 @@
 import csv
 
-def read_sales_from_csv(file_path):
+def read_sales_from_csv(path, field_map):
     sales = []
-    with open(file_path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
+    with open(path, newline='', encoding="utf-8") as file:
+        reader = csv.DictReader(file)
         for row in reader:
-            row["qty"] = int(row["qty"])
-            row["price"] = float(row["price"])
-            sales.append(row)
+            sales.append({
+                "name": row.get(field_map["name"], ""),
+                "qty": int(row.get(field_map["qty"], 0)),
+                "price": float(row.get(field_map["price"], 0)),
+                "timestamp": row.get(field_map["timestamp"], "")
+            })
     return sales
