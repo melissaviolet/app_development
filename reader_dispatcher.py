20ef53405c06ef6951585af1a3010f583e75c481
Melissa Violet <melissavioletvs@gmail.com>
2025-07-23 23:35:11 +0300

    consistent casing in the content saved in sourse_type

--- reader_dispatcher.py
+++ reader_dispatcher.py
@@ -9,11 +9,11 @@ def read_sales(config):
 
     if source_type == "CSV":
         return read_sales_from_csv(config["csv_path"], field_map)
-    elif source_type == "sqlite":
+    elif source_type == "SQLITE":
         return read_sales_from_sqlite(config["sqlite_path"], field_map)
-    elif source_type == "mysql":
+    elif source_type == "MYSQL":
         return read_sales_from_mysql(config["mysql"], field_map)
-    elif source_type == "api":
+    elif source_type == "API":
         return read_sales_from_api(config["api_url"], field_map, config.get("api_token"))
     else:
-        raise ValueError("Unsupported source type")
+        raise ValueError(f"Unsupported source type:{source_type}")
